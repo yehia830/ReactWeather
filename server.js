2517de20ce0143c9a97e0bf64bc3b3229541704e
Yehia Abdullah <Yehia830@MC.local>
2017-07-14 22:40:04 -0400

    getting ready to deploy

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 //create app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req,res,next) {
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+})
+
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up');
+
+
+app.listen(PORT, function () {
+  console.log('Express server is up' + port);
 });
