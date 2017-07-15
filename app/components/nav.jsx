6531ec4355f723ef9e3567e717d608dcc6e519cf
Yehia Abdullah <Yehia830@MC.local>
2017-07-15 19:05:49 -0400

    style nav bar

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -1,17 +1,45 @@
 const React = require('react');
 var {Link, IndexLink} = require('react-router');
 
+var Nav = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    alert('Not yet wired up!')
+  },
+  render: function(){
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>View Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+            <form onSubmit={this.onSearch}>
+                <ul className="menu">
+                  <li>
+                    <input type="search" placeholder="Search Weather"/>
+                  </li>
+                  <li>
+                    <input type="submit" className="button" value="Get Weather"/>
+                  </li>
+                </ul>
+            </form>
+        </div>
+      </div>
 
+    );
+  }
 
-var Nav = (props) => {
-  return(
-    <div>
-    <h2>Nav Component</h2>
-    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink><br></br>
-    <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link><br></br>
-    <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>View Examples</Link><br></br>
-    </div>
-  )}
-
+})
 
 module.exports = Nav;
